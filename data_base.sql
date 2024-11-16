5ad49ff1821cfffee818dec82d9d3b6c41673a06
Monroebu <monroebu@student.21-school.ru>
2024-11-16 18:02:50 +0500

    correct tables

--- data_base.sql
+++ data_base.sql
@@ -1,46 +1,41 @@
-CREATE TABLE "objects_type"(   
-  id SERIAL primary key,
+CREATE TABLE "objects_type" (   
+  id SERIAL PRIMARY KEY,
   name VARCHAR(42)
 );
 
-CREATE TABLE objects(   
-  id SERIAL primary key,
-  name VARCHAR(42) not null default 0,
-  type INTEGER not null default 0,
-  constraint fk_objects_type foreign key (type) objects_type(id)
+CREATE TABLE objects (   
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(42),
+  type INTEGER NOT NULL DEFAULT 0,
+  CONSTRAINT fk_objects_type FOREIGN KEY (type) REFERENCES "objects_type"(id)
 );
 
-CREATE TABLE wells(   
-  well INTEGER primary key,
-  ngdu INTEGER not null default 0,
-  cdng INTEGER not null default 0,
-  kust INTEGER not null default 0,
-  mest INTEGER not null default 0
+CREATE TABLE wells (   
+  well INTEGER PRIMARY KEY,
+  ngdu INTEGER NOT NULL DEFAULT 0,
+  cdng INTEGER NOT NULL DEFAULT 0,
+  kust INTEGER NOT NULL DEFAULT 0,
+  mest INTEGER NOT NULL DEFAULT 0
 );
 
-СREATE TABLE well_day_histories(   
-  well INTEGER not null,
-  date_fact date not null default current_date primary key,
-  debit FLOAT not null default 0,
-  ee_consume FLOAT not null default 0,
-  expenses FLOAT not null default 0,
-  pump_operating FLOAT not null default 0,
-  constraint fk_well_day_histories_well foreign key (well) references wells(well)
+CREATE TABLE well_day_histories (   
+  well INTEGER NOT NULL,
+  date_fact DATE NOT NULL DEFAULT current_date PRIMARY KEY,
+  debit FLOAT NOT NULL DEFAULT 0,
+  ee_consume FLOAT NOT NULL DEFAULT 0,
+  expenses FLOAT NOT NULL DEFAULT 0,
+  pump_operating FLOAT NOT NULL DEFAULT 0,
+  CONSTRAINT fk_well_day_histories_well FOREIGN KEY (well) REFERENCES wells(well)
 );
 
-СREATE TABLE well_day_plans(   
-  well INTEGER not null,
-  date_plan date not null default current_date primary key,
-  debit FLOAT not null default 0,
-  ee_consume FLOAT not null default 0,
-  expenses FLOAT not null default 0,
-  pump_operating FLOAT not null default 0,
-  constraint fk_well_day_histories_well foreign key (well) references wells(well)
+CREATE TABLE well_day_plans (   
+  well INTEGER NOT NULL,
+  date_plan DATE NOT NULL DEFAULT current_date PRIMARY KEY,
+  debit FLOAT NOT NULL DEFAULT 0,
+  ee_consume FLOAT NOT NULL DEFAULT 0,
+  expenses FLOAT NOT NULL DEFAULT 0,
+  pump_operating FLOAT NOT NULL DEFAULT 0,
+  CONSTRAINT fk_well_day_plans_well FOREIGN KEY (well) REFERENCES wells(well)
 );
 
--- insert into PROG_TEST_PEOPLE values (1, 'Anna', 166);
--- insert into PROG_TEST_PEOPLE values (2, 'Andrey', 211);
--- insert into PROG_TEST_PEOPLE values (3, 'Kate', 333);
--- insert into PROG_TEST_PEOPLE values (4, 'Denis', 131);
-
---SELECT *;
+SELECT * FROM wells;
